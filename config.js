5af7f239ce49dbe4fdd4ec8a075c265b51c683a2
Daniel Mulugeta <133869018+DanielMD1989@users.noreply.github.com>
2026-06-23 15:17:26 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // ============================================================
 window.ABISE_CONFIG = {
   SUPABASE_URL: "https://zttjyldtutididisppzs.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_T6EmZOXgnln_16s9RKdVoA_wKf-l_wl"
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp0dGp5bGR0dXRpZGlkaXNwcHpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMDU5NDYsImV4cCI6MjA5Nzc4MTk0Nn0.8DEDwUkpXxQoTF2kAOfWhh1x3nNJn4hi1wwL9-WXCi0"
 };
